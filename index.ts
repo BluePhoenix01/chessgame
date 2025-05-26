1a78c2abf40061fd1b19ec4e58727d53bd7d2d45
BluePhoenix01 <hi2a@ymail.com>
2025-05-25 20:05:25 -0400

    Server Initialization

--- index.ts
+++ index.ts
@@ -5,11 +5,29 @@ const app = express()
 const port = 3000
 // const PATH = path.dirname(fileURLToPath(import.meta.url))
 app.use(morgan('tiny'))
+app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.send("Hello! World")
+  res.send("Hello World")
 })
 
+app.get('/user/:id', (req, res) => {
+    const id = req.params.id
+    res.send(`Fetching user with id: ${id}`) 
+})
+
+app.post('/user', (req, res) => {
+    const userdata = req.body
+    console.log(userdata)
+    res.send(`Creating user: ${JSON.stringify(userdata)}`) 
+})
+
+app.put('/user/:id', (req, res) => {
+    const id = req.params.id
+    const userdata = req.body
+    res.send(`Updating user ${id}: ${JSON.stringify(userdata)}`) 
+})
+ 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
